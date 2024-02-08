fb5e7e7cf1fc991ce003027920e821bf861ced51
Lexxie Raven Black <30494658+ladylexxie@users.noreply.github.com>
2024-02-08 17:16:28 +0000

    UI Testing

--- Assets/Scripts/Player/CameraControl.cs
+++ Assets/Scripts/Player/CameraControl.cs
@@ -7,9 +7,11 @@ public class CameraControl : MonoBehaviour {
     [Range(1, 89)]
     public int maxX = 60;
     public Camera cam;
+    public GameObject settingsUI;
 
     float rotX = 0f;
     float rotY = 0f;
+    bool paused = false;
 
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
@@ -17,12 +19,21 @@ public class CameraControl : MonoBehaviour {
     }
 
     void Update() {
-        rotY += Input.GetAxis("Mouse X") * sensivity;
-        rotX += Input.GetAxis("Mouse Y") * sensivity;
+        if (!paused) {
+            rotY += Input.GetAxis("Mouse X") * sensivity;
+            rotX += Input.GetAxis("Mouse Y") * sensivity;
 
-        rotX = Mathf.Clamp(rotX, minX, maxX);
+            rotX = Mathf.Clamp(rotX, minX, maxX);
 
-        cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
-        transform.rotation = Quaternion.Euler(0, rotY, 0);
+            cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+            transform.rotation = Quaternion.Euler(0, rotY, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P)) {
+            paused = !paused;
+            Cursor.lockState = paused ? CursorLockMode.None : CursorLockMode.Locked;
+            Cursor.visible = paused;
+            settingsUI.SetActive(paused);
+        }
     }
 }
