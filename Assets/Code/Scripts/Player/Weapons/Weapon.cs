92bce1092c01cd249ca17e243c0321953cc4e4c1
George Robson <grobson97@gmail.com>
2024-02-19 09:25:19 +0000

    Added weapon models

--- Assets/Code/Scripts/Player/Weapons/Weapon.cs
+++ Assets/Code/Scripts/Player/Weapons/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : ScriptableObject {
     [TableColumnWidth(90, Resizable = false)]
     [DisableIf("isLocked")]
     public GameObject weaponObject;
+
     [VerticalGroup("Weapon Stats")]
     [TableColumnWidth(70)]
     [ProgressBar(1, 100)]
@@ -16,18 +17,21 @@ public class Weapon : ScriptableObject {
     [PropertyTooltip("Base Damage")]
     [DisableIf("isLocked")]
     public int baseDamage;
+
     [VerticalGroup("Weapon Stats")]
     [ProgressBar(1f, 10f)]
     [LabelText("S. Multiplier")]
     [PropertyTooltip("Speed Damage Multiplier")]
     [DisableIf("isLocked")]
     public float speedDamageMultiplier;
+
     [VerticalGroup("Weapon Stats")]
     [ProgressBar(0f, 10f)]
     [LabelText("A. Speed")]
     [PropertyTooltip("Attack Speed")]
     [DisableIf("isLocked")]
     public float attackSpeed;
+
     [VerticalGroup("Weapon Stats")]
     [ProgressBar(0f, 10f)]
     [LabelText("SA. Cooldown")]
@@ -39,7 +43,7 @@ public class Weapon : ScriptableObject {
 
     [Button("", ButtonSizes.Large, Icon = SdfIconType.LockFill, Stretch = false)]
     [ShowIf("IsLocked")]
-    [VerticalGroup("Lock Weapon")]
+    [VerticalGroup("Lock")]
     [TableColumnWidth(40, Resizable = false)]
     private void UnlockButton()
     {
@@ -48,7 +52,7 @@ public class Weapon : ScriptableObject {
 
     [Button("", ButtonSizes.Large, Icon = SdfIconType.UnlockFill, Stretch = false)]
     [HideIf("IsLocked")]
-    [VerticalGroup("Lock Weapon")]
+    [VerticalGroup("Lock")]
     private void LockButton()
     {
         isLocked = true;
--- Assets/Code/Scripts/Player/Weapons/WeaponsController.cs
+++ Assets/Code/Scripts/Player/Weapons/WeaponsController.cs
@@ -1,15 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Sirenix.OdinInspector;
+using UnityEngine;
 
 public class WeaponsController : MonoBehaviour
 {
+    private Weapon activeWeapon;
+    int currentWeaponIndex;
 
     [TableList(AlwaysExpanded = true)]
     public List<Weapon> weapons;
+    private List<GameObject> weaponObjects;
 
-    private void HandleInput() {
+    /// <summary>
+    /// Instantiates weapons and sets the active weapon
+    /// </summary>
+    private void Start()
+    {
+        // Loop through each weapon
+        foreach (Weapon weapon in weapons)
+        {
+            // Instantiate the weapon object as a child of the current transform
+            GameObject instance = Instantiate(weapon.weaponObject, transform);
+            weaponObjects.Add(instance);
+            // Deactivate the weapon object
+            weapon.weaponObject.SetActive(false);
+            // If no active weapon is set, set the current weapon as the active weapon
+            if (activeWeapon == null)
+            {
+                activeWeapon = weapon;
+                // Activate the weapon object
+                weapon.weaponObject.SetActive(true);
+            }
+        }
+    }
 
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetAxis("Cycle Weapons") > 0)
+        {
+            CycleToNextWeapon();
+        }
+        if (Input.GetAxis("Cycle Weapons") < 0)
+        {
+            CycleToPreviousWeapon();
+        }
+    }
+
+    private void CycleToNextWeapon()
+    {
+        currentWeaponIndex = weapons.IndexOf(activeWeapon);
+        currentWeaponIndex = (currentWeaponIndex + 1) % weapons.Count;
+        activeWeapon = weapons[currentWeaponIndex];
+    }
+
+    private void CycleToPreviousWeapon()
+    {
+        currentWeaponIndex = weapons.IndexOf(activeWeapon);
+        currentWeaponIndex = ((currentWeaponIndex - 1) + weapons.Count) % weapons.Count;
+        activeWeapon = weapons[currentWeaponIndex];
     }
 }
