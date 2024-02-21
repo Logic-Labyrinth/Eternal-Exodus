073a1af373bfc1b8ba481b410cb1cabc41c45e20
Lexxie Raven Black <lexxie.raven.black@proton.me>
2024-02-21 11:16:43 +0000

    Weapon Cooldowns

--- Assets/Code/Scripts/Player/Weapons/WeaponsController.cs
+++ Assets/Code/Scripts/Player/Weapons/WeaponsController.cs
@@ -3,104 +3,124 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-public class WeaponsController : MonoBehaviour
-{
-    private Weapon activeWeapon;
+struct WeaponObject {
+    public GameObject weaponObj;
+    public Weapon weapon;
+    public bool canUseSpecialAttack;
+    public float cooldown;
+
+    public WeaponObject(GameObject weaponObj, Weapon weapon) {
+        this.weaponObj = weaponObj;
+        this.weaponObj.SetActive(false);
+        this.weapon = weapon;
+        canUseSpecialAttack = true;
+        cooldown = weapon.specialAttackCooldown;
+    }
+}
+
+public class WeaponsController : MonoBehaviour {
+    // private Weapon activeWeapon;
+    private int activeWeaponIndex;
     [SerializeField]
     private GameObject hand;
-    int currentWeaponIndex;
+    // int currentWeaponIndex;
 
     [TableList(AlwaysExpanded = true)]
     public List<Weapon> weapons;
     [SerializeField]
-    private List<GameObject> weaponObjects;
+    // private List<GameObject> weaponObjects;
+    private List<WeaponObject> weaponObjects;
     [SerializeField]
     private GameObject playerReference;
 
     /// <summary>
     /// Instantiates weapons and sets the active weapon
     /// </summary>
-    private void Start()
-    {
+    private void Start() {
+        activeWeaponIndex = 0;
+        weaponObjects = new List<WeaponObject>();
         // Loop through each weapon to create game objects
-        foreach (Weapon weapon in weapons)
-        {
+        for (int i = 0; i < weapons.Count; i++) {
+            var weapon = weapons[i];
             // Create game object
             GameObject weaponObject = Instantiate(weapon.weaponObject);
             // Set parent
             weaponObject.transform.SetParent(hand.transform);
-            // Set position
-            weaponObject.transform.localPosition = weapon.localPosition;
-            // Set rotation
-            weaponObject.transform.localRotation = weapon.localRotation;
+            // Set position & rotation
+            weaponObject.transform.SetLocalPositionAndRotation(weapon.localPosition, weapon.localRotation);
             // Add to weapon objects list
-            weaponObjects.Add(weaponObject);
-            // Set all inactive
-            weaponObject.SetActive(false);
-            // Set first weapon active
-            if (weapon == weapons[0]) {
-                activeWeapon = weapon;
-                currentWeaponIndex = 0;
-                weaponObject.SetActive(true);
-            }
+            weaponObjects.Add(new WeaponObject(weaponObject, weapon));
         }
+
+        weaponObjects[activeWeaponIndex].weaponObj.SetActive(true);
     }
 
-    private void Update()
-    {
+    private void Update() {
         HandleInput();
     }
 
-    private void HandleInput()
-    {
-        if (Input.GetAxis("Cycle Weapons") > 0 || Input.GetKeyDown(KeyCode.E))
-        {
-            CycleToNextWeapon();
-        }
-        if (Input.GetAxis("Cycle Weapons") < 0 || Input.GetKeyDown(KeyCode.Q))
-        {
-            CycleToPreviousWeapon();
-        }
-        if (Input.GetButtonDown("Select Weapon 1")) {
-            SetActiveWeapon(0);
-        }
-        if (Input.GetButtonDown("Select Weapon 2")) {
-            SetActiveWeapon(1);
-        }
-        if (Input.GetButtonDown("Select Weapon 3")) {
-            SetActiveWeapon(2);
-        }
-        if (Input.GetButtonDown("Basic Attack")) {
-            activeWeapon.BasicAttack(playerReference);
-        }
+    private void HandleInput() {
+        var currentWeapon = weaponObjects[activeWeaponIndex];
+        if (Input.GetAxis("Cycle Weapons") > 0 || Input.GetKeyDown(KeyCode.E)) CycleToNextWeapon();
+        if (Input.GetAxis("Cycle Weapons") < 0 || Input.GetKeyDown(KeyCode.Q)) CycleToPreviousWeapon();
+
+        if (Input.GetButtonDown("Select Weapon 1")) SetActiveWeapon(0);
+        if (Input.GetButtonDown("Select Weapon 2")) SetActiveWeapon(1);
+        if (Input.GetButtonDown("Select Weapon 3")) SetActiveWeapon(2);
+
+        if (Input.GetButtonDown("Basic Attack"))
+            currentWeapon.weapon.BasicAttack(playerReference);
+        // activeWeapon.BasicAttack(playerReference);
+
         if (Input.GetButtonDown("Special Attack")) {
-            activeWeapon.SpecialAttack(playerReference);
+            Debug.Log(currentWeapon.canUseSpecialAttack);
+            if (!currentWeapon.canUseSpecialAttack) return;
+            // Debug.Log("Special Attack");
+            // activeWeapon.canUseSpecialAttack = false;
+            // activeWeapon.SpecialAttack(playerReference);
+            currentWeapon.canUseSpecialAttack = false;
+            Debug.Log(currentWeapon.canUseSpecialAttack);
+            currentWeapon.weapon.SpecialAttack(playerReference);
+            StartCoroutine(ResetSpecialAbility(currentWeapon));
         }
+
         if (Input.GetButtonUp("Special Attack")) {
-            activeWeapon.SpecialRelease(playerReference);
+            currentWeapon.weapon.SpecialAttack(playerReference);
+            // activeWeapon.SpecialRelease(playerReference);
         }
     }
 
     private void SetActiveWeapon(int index) {
-        weaponObjects[currentWeaponIndex].SetActive(false);
-        currentWeaponIndex = index;
-        activeWeapon = weapons[currentWeaponIndex];
-        weaponObjects[currentWeaponIndex].SetActive(true);
+        var currentWeapon = weaponObjects[activeWeaponIndex];
+        currentWeapon.weaponObj.SetActive(false);
+        activeWeaponIndex = index;
+        // activeWeapon = weapons[activeWeaponIndex];
+        currentWeapon = weaponObjects[activeWeaponIndex];
+        currentWeapon.weaponObj.SetActive(true);
     }
 
-    private void CycleToNextWeapon()
-    {
-        weaponObjects[currentWeaponIndex].SetActive(false);
-        currentWeaponIndex = (currentWeaponIndex + 1) % weapons.Count;
-        activeWeapon = weapons[currentWeaponIndex];
-        weaponObjects[currentWeaponIndex].SetActive(true);
+    private void CycleToNextWeapon() {
+        var currentWeapon = weaponObjects[activeWeaponIndex];
+        currentWeapon.weaponObj.SetActive(false);
+        activeWeaponIndex = (activeWeaponIndex + 1) % weapons.Count;
+        // activeWeapon = weapons[activeWeaponIndex];
+        currentWeapon = weaponObjects[activeWeaponIndex];
+        currentWeapon.weaponObj.SetActive(true);
     }
 
-    private void CycleToPreviousWeapon()
-    {
-        weaponObjects[currentWeaponIndex].SetActive(false);
-        currentWeaponIndex = ((currentWeaponIndex - 1) + weapons.Count) % weapons.Count;
-        activeWeapon = weapons[currentWeaponIndex];
-        weaponObjects[currentWeaponIndex].SetActive(true);
+    private void CycleToPreviousWeapon() {
+        var currentWeapon = weaponObjects[activeWeaponIndex];
+        currentWeapon.weaponObj.SetActive(false);
+        activeWeaponIndex = (activeWeaponIndex - 1 + weapons.Count) % weapons.Count;
+        // activeWeapon = weapons[activeWeaponIndex];
+        currentWeapon = weaponObjects[activeWeaponIndex];
+        currentWeapon.weaponObj.SetActive(true);
+    }
+
+    IEnumerator ResetSpecialAbility(WeaponObject weaponObject) {
+        Debug.Log("Waiting");
+        yield return new WaitForSeconds(weaponObject.cooldown);
+        Debug.Log("Something");
+        // weaponObject.canUseSpecialAttack = true;
     }
 }
