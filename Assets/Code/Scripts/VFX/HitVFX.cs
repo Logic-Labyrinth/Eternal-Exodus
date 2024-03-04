38b6e6e6476e4ba17421c5c03509c42595beba73
Lexxie Raven Black <30494658+ladylexxie@users.noreply.github.com>
2024-03-04 13:42:09 +0000

    Formatting stupidity

--- Assets/Code/Scripts/VFX/HitVFX.cs
+++ Assets/Code/Scripts/VFX/HitVFX.cs
@@ -4,6 +4,11 @@ using UnityEngine.VFX;
 public class HitVFX : MonoBehaviour {
   [SerializeField] VisualEffect vfx;
   [SerializeField, Range(0, 10)] int lifespan = 5;
+  [SerializeField] float sizeMultiplier = 1;
+
+  void Start() {
+    gameObject.transform.localScale = sizeMultiplier * Vector3.one;
+  }
 
   public void Play() {
     Invoke(nameof(Stop), lifespan);
