18600ae91da39c1f97655881cfb1947581db6fc1
Lexxie Raven Black <30494658+ladylexxie@users.noreply.github.com>
2024-03-14 13:14:16 +0000

    Audio/prefab misc

--- Assets/Resources/Code/Scripts/Audio/AudioManager.cs
+++ Assets/Resources/Code/Scripts/Audio/AudioManager.cs
@@ -1,5 +1,28 @@
+using System;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
     [SerializeField] Sound[] sounds;
+
+    void Awake() {
+        foreach(Sound s in sounds){
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    /*
+    FindObjectOfType<AudioManager>().Play("name");
+    */
+    public void Play(string name){
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null){
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
 }
