aefd5ff0448745f6a0057b62ec73344798af281c
Lexxie Raven Black <30494658+ladylexxie@users.noreply.github.com>
2024-03-14 11:55:25 +0000

    Audio start + torch sounds

--- Assets/Resources/Code/Scripts/Audio/AudioManager.cs
+++ Assets/Resources/Code/Scripts/Audio/AudioManager.cs
@@ -1,17 +1,5 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class AudioManager : MonoBehaviour {
-    public static AudioManager instance;
-    public AudioSource audioSource;
-    public AudioClip[] audioClips;
-
-    private void Awake() {
-        if (instance == null) instance = this;
-    }
-
-    public void PlayAudio(int index) {
-        audioSource.clip = audioClips[index];
-        audioSource.Play();
-    }
+    [SerializeField] Sound[] sounds;
 }
