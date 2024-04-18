da70f344d96d4b0a04036a7d620287a266eb8e2f
Lexxie Raven Black <u2061581@unimail.hud.ac.uk>
2024-04-18 12:40:12 +0100

    I'm a Physician
    That's Kyle, not me :)

--- Packages/com.alexismorin.editorphysicssimulation@1.0.0/Editor/simulateEditorPhysics.cs
+++ Packages/com.alexismorin.editorphysicssimulation@1.0.0/Editor/simulateEditorPhysics.cs
@@ -1,28 +1,63 @@
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 public class simulateEditorPhysics : MonoBehaviour {
-    void Awake(){
+    void Awake() {
         // Physics.simulationMode = SimulationMode.Script;
         Physics.simulationMode = SimulationMode.FixedUpdate;
     }
 
-    [MenuItem ("Tools/Simulate Physics in Editor - Start")]
-    static void EditorSimStart () {
+    [MenuItem("Tools/Simulate Physics in Editor - Start x1")]
+    static void EditorSimStart1() {
         Debug.Log("Simulating");
         Physics.simulationMode = SimulationMode.Script;
+
+        FindObjectsOfType<Rigidbody>()
+            .ToList()
+            .ForEach(r => r.velocity = Vector3.zero);
+
+        Time.timeScale = 1f;
         // Physics.simulationMode = SimulationMode.FixedUpdate;
         EditorApplication.update += Update;
     }
 
-    [MenuItem ("Tools/Simulate Physics in Editor - Stop")]
-    static void EditorSimStop () {
+    [MenuItem("Tools/Simulate Physics in Editor - Start x10")]
+    static void EditorSimStart2() {
+        Debug.Log("Simulating");
+        Physics.simulationMode = SimulationMode.Script;
+
+        FindObjectsOfType<Rigidbody>()
+            .ToList()
+            .ForEach(r => r.velocity = Vector3.zero);
+
+        Time.timeScale = 0.1f;
+        // Physics.simulationMode = SimulationMode.FixedUpdate;
+        EditorApplication.update += Update;
+    }
+
+    [MenuItem("Tools/Simulate Physics in Editor - Start x100")]
+    static void EditorSimStart3() {
+        Debug.Log("Simulating");
+        Physics.simulationMode = SimulationMode.Script;
+
+        FindObjectsOfType<Rigidbody>()
+            .ToList()
+            .ForEach(r => r.velocity = Vector3.zero);
+
+        Time.timeScale = 0.01f;
+        // Physics.simulationMode = SimulationMode.FixedUpdate;
+        EditorApplication.update += Update;
+    }
+
+    [MenuItem("Tools/Simulate Physics in Editor - Stop")]
+    static void EditorSimStop() {
         Debug.Log("Not Simulating");
         // Physics.simulationMode = SimulationMode.FixedUpdate;
         EditorApplication.update -= Update;
     }
 
-    static void Update () {
+    static void Update() {
         Physics.Simulate(Time.deltaTime);
     }
 }
