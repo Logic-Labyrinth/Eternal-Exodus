c00c7019b048ae4fbb5cf568a0ad553553c3f42d
George Robson <grobson97@gmail.com>
2024-04-30 13:59:13 +0100

    PAwn go brrr

--- Assets/Resources/Code/Scripts/Player/Weapons/SpearAbility.cs
+++ Assets/Resources/Code/Scripts/Player/Weapons/SpearAbility.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SpecialAbilityDamage : MonoBehaviour {
     [SerializeField] Weapon spearData;
@@ -12,6 +13,11 @@ public class SpecialAbilityDamage : MonoBehaviour {
         if (other.gameObject.layer != LayerMask.NameToLayer("Enemy")) return;
         if (!pm.dashing) return;
         other.GetComponent<HealthSystem>().TakeDamage(spearData.baseDamage, WeaponDamageType.SPEAR, transform.position);
+        other.GetComponent<NavMeshAgent>().isStopped = true;
+        other.GetComponent<NavMeshAgent>().updatePosition = false;
+
+        // add force away from spear position and up
+        other.GetComponent<Rigidbody>().AddForce((other.transform.position - transform.position).normalized * 5 + Vector3.up * 15, ForceMode.Impulse);
         // other.transform.parent.GetComponent<HealthSystem>().TakeDamage(spearData.baseDamage, WeaponDamageType.SPEAR, transform.position);
     }
 }
