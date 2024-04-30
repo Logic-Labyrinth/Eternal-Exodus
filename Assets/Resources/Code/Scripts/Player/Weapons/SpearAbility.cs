1b781ba32df807f33a8e767bf8ac0a88f882a246
Lexxie Raven Black <u2061581@unimail.hud.ac.uk>
2024-04-30 13:34:05 +0100

    Spear Fix

--- Assets/Resources/Code/Scripts/Player/Weapons/SpearAbility.cs
+++ Assets/Resources/Code/Scripts/Player/Weapons/SpearAbility.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class SpecialAbilityDamage : MonoBehaviour {
     [SerializeField] Weapon spearData;
+    PlayerMovement pm;
+
+    void Start() {
+        pm = GameObject.Find("Player").GetComponent<PlayerMovement>();
+    }
 
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.layer != LayerMask.NameToLayer("Enemy")) return;
+        if (!pm.dashing) return;
         other.GetComponent<HealthSystem>().TakeDamage(spearData.baseDamage, WeaponDamageType.SPEAR, transform.position);
         // other.transform.parent.GetComponent<HealthSystem>().TakeDamage(spearData.baseDamage, WeaponDamageType.SPEAR, transform.position);
     }
