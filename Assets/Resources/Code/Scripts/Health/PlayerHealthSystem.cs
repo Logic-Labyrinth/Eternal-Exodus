72f7e56ed40071d18939491144f93fe771326fc1
Lexxie Raven Black <u2061581@unimail.hud.ac.uk>
2024-04-30 15:25:25 +0100

    Loading screen replacement

--- Assets/Resources/Code/Scripts/Health/PlayerHealthSystem.cs
+++ Assets/Resources/Code/Scripts/Health/PlayerHealthSystem.cs
@@ -36,11 +36,11 @@ public class PlayerHealthSystem : MonoBehaviour {
         }
     }
 
-    // private void Update() {
-    //     if(Input.GetKeyDown(KeyCode.Z)) Shield();
-    //     if(Input.GetKeyDown(KeyCode.X)) DamageShield();
-    //     if(Input.GetKeyDown(KeyCode.C)) BreakShield();
-    // }
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Z)) Shield();
+        if(Input.GetKeyDown(KeyCode.X)) DamageShield();
+        if(Input.GetKeyDown(KeyCode.C)) BreakShield();
+    }
 
     public void Kill() {
         // gameObject.SetActive(false);
