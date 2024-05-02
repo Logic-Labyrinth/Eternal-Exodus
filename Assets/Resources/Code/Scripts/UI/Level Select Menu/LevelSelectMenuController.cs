05592ff9995844821b9b3d36a4824f58c8847511
Lexxie Raven Black <u2061581@unimail.hud.ac.uk>
2024-05-02 16:41:06 +0100

    a lotta stuff idk

--- Assets/Resources/Code/Scripts/UI/Level Select Menu/LevelSelectMenuController.cs
+++ Assets/Resources/Code/Scripts/UI/Level Select Menu/LevelSelectMenuController.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class LevelSelectMenuController : MonoBehaviour {
-    static GameObject buttonGO;
     GameManager gameManager;
 
     public static LevelSelectMenuController Instance { get; private set; }
@@ -12,22 +10,4 @@ public class LevelSelectMenuController : MonoBehaviour {
         else Instance = this;
         if (!gameManager) gameManager = GameManager.Instance;
     }
-
-    // public static void OnPointerEnter(BaseEventData eventData) {
-    //     buttonGO = (eventData as PointerEventData).pointerCurrentRaycast.gameObject;
-    //     if (!buttonGO.CompareTag("Seed Select Button")) return;
-        // if (!buttonGO.TryGetComponent<UnityEngine.UI.Outline>(out var outline)) {
-        //     Debug.Log("Outline not found on " + buttonGO);
-        //     return;
-        // }
-        // outline.enabled = true;
-    // }
-
-    // public static void OnPointerExit(BaseEventData eventData) {
-        // if (!buttonGO.TryGetComponent<UnityEngine.UI.Outline>(out var outline)) {
-        //     Debug.Log("Outline not found on " + buttonGO);
-        //     return;
-        // }
-        // outline.enabled = false;
-    // }
 }
