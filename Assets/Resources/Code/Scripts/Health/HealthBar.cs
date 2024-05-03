5ea830bc94fd5c2eb01d65fa1ce860d313f206e0
Lexxie Raven Black <u2061581@unimail.hud.ac.uk>
2024-05-03 14:35:56 +0100

    Health bars + other fixes

--- Assets/Resources/Code/Scripts/Health/HealthBar.cs
+++ Assets/Resources/Code/Scripts/Health/HealthBar.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthBar : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class HealthBar : MonoBehaviour {
+    Material material;
+    Transform playerTransform;
+
+    private void Awake() {
+        playerTransform = Camera.main.transform;
+        material = GetComponent<Renderer>().materials[0];
+        material.SetFloat("_Progress", 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void SetProgress(float progress) {
+        material.SetFloat("_Progress", progress);
+    }
+
+    void LateUpdate() {
+        transform.LookAt(new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z));
+        transform.Rotate(Vector3.right, -90);
     }
 }
