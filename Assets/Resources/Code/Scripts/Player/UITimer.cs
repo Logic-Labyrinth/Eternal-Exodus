90f5881e15a6b98bf5f09f6e33efecd87eaaf4af
Lexxie Raven Black <30494658+ladylexxie@users.noreply.github.com>
2024-05-22 17:04:06 +0100

    Tornado shrinking + killing player

--- Assets/Resources/Code/Scripts/Player/UITimer.cs
+++ Assets/Resources/Code/Scripts/Player/UITimer.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class UITimer : MonoBehaviour {
     public static UITimer Instance;
@@ -9,18 +10,27 @@ public class UITimer : MonoBehaviour {
     [SerializeField] int secondsPerPawn = 5;
     [SerializeField] int secondsPerRook = 5;
     [SerializeField] int secondsPerBishop = 5;
-    // float scale = 1f;
+    float scale = 1f;
     float timer = 0;
+    VisualEffect tornadoVFX;
+    GameObject tornadoTrigger;
+    Vector3 tornadoTriggerScale;
 
     void Awake() {
         if (Instance != null && Instance != this) Destroy(this);
         else Instance = this;
+        tornadoVFX = GameObject.Find("VG_Tornado").GetComponent<VisualEffect>();
+        tornadoTrigger = GameObject.Find("VG_Tornado_Trigger");
+        tornadoTriggerScale = tornadoTrigger.transform.localScale;
     }
 
     void FixedUpdate() {
         timer += Time.fixedDeltaTime;
         float timeLeft = countdownTimeSeconds - timer;
-        // scale = timeLeft / countdownTimeSeconds;
+        scale = timeLeft / countdownTimeSeconds;
+
+        tornadoVFX.SetFloat("Size", scale * 100);
+        tornadoTrigger.transform.localScale = tornadoTriggerScale * scale;
         timerText.text = ((int)timeLeft / 60).ToString() + ":" + ((int)timeLeft % 60).ToString("D2");
     }
 
