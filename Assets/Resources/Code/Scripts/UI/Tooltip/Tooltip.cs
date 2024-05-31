f8e55f3bcad38829be9bfdedabe39e30434ceefc
George Robson <grobson97@gmail.com>
2024-05-31 19:48:50 +0100

    Added video and tooltips to enemies

---
+++ Assets/Resources/Code/Scripts/UI/Tooltip/Tooltip.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Tooltip : MonoBehaviour
+{
+    public TextMeshProUGUI tooltipText;
+    public static Tooltip Instance { get; private set; }
+
+    void Awake() {
+        // create instance
+        if (Instance != null && Instance != this) Destroy(this);
+        else Instance = this;
+
+        // hide tooltip
+        gameObject.SetActive(false);
+    }
+
+    public void ShowTooltip(string text) {
+        transform.position = Input.mousePosition;
+        tooltipText.text = text;
+        gameObject.SetActive(true);
+    }
+
+    public void HideTooltip() {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // follow mouse in canvas
+        transform.position = Input.mousePosition;
+    }
+}
