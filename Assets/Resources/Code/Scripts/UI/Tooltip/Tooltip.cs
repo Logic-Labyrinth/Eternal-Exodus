fe2b5884f217c7c4087f3739cf490be9f3acc917
Lexxie Raven Black <30494658+ladylexxie@users.noreply.github.com>
2024-06-05 23:02:06 +0100

    Fixed window scaling

--- Assets/Resources/Code/Scripts/UI/Tooltip/Tooltip.cs
+++ Assets/Resources/Code/Scripts/UI/Tooltip/Tooltip.cs
@@ -5,12 +5,15 @@ public class Tooltip : MonoBehaviour {
     public TextMeshProUGUI tooltipText;
     public static Tooltip Instance { get; private set; }
     float screenHeight;
+    float xDiff, yDiff;
 
     void Awake() {
         // create instance
         if (Instance != null && Instance != this) Destroy(this);
         else Instance = this;
         screenHeight = Screen.height;
+        xDiff = Screen.width / 1920f;
+        yDiff = Screen.height / 1080f;
 
         // hide tooltip
         gameObject.SetActive(false);
@@ -18,7 +21,12 @@ public class Tooltip : MonoBehaviour {
 
     public void ShowTooltip(string text) {
         Vector3 mousePos = Input.mousePosition;
+        Debug.Log(mousePos);
         mousePos.y -= screenHeight;
+
+        mousePos.x /= xDiff;
+        mousePos.y /= yDiff;
+
         GetComponent<RectTransform>().anchoredPosition = mousePos;
 
         tooltipText.text = text;
@@ -34,6 +42,10 @@ public class Tooltip : MonoBehaviour {
         // follow mouse in canvas
         Vector3 mousePos = Input.mousePosition;
         mousePos.y -= screenHeight;
+
+        mousePos.x /= xDiff;
+        mousePos.y /= yDiff;
+
         mousePos += new Vector3(25, -25, 0);
         GetComponent<RectTransform>().anchoredPosition = mousePos;
     }
